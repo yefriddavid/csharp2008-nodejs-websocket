37ffdadd1f9a31771a3c49d1e0d3ef1b713209db
david rios <yefriddavid@gmail.com>
2019-03-21 20:11:20 +0000

    first commit

---
+++ ExampleSocketClient2008/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+
+namespace ExampleSocketClient2008
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            
+            // WebSocketTest ws = new WebSocketTest("wss://192.168.0.13:8080");
+            //WebSocketTest ws = new WebSocketTest("wss://demos.kaazing.com/echo");
+            WebSocketTest ws = new WebSocketTest("ws://echo.websocket.org/");
+
+            ws.onNotification += onNotifications;
+            ws.onNewMessage += onNewMessage;     
+            ws.conectar();
+            ws.send("Hello, I  am VS2008!");
+
+            
+        }
+        static void onNewMessage(String message)
+        {
+            Console.WriteLine("Arrived new message: " + message);
+        }
+        static void onNotifications(String message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+}
---
+++ ExampleSocketClient2008/WebSocketTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Rebex.Net;
+
+namespace ExampleSocketClient2008
+{
+
+    public class WebSocketTest
+    {
+        
+        public delegate void dgEventRaiser(String message);
+        public event dgEventRaiser onNotification;
+        
+        public delegate void dgEventNewMessage(String message);
+        public event dgEventNewMessage onNewMessage;
+
+        String uri;
+        public WebSocketClient client;
+        public WebSocketTest(string uri)
+        {
+            this.uri = uri;
+            this.client = new WebSocketClient();            
+        }
+        public void send(String message)
+        {
+            this.onNotification("Sending message...");
+            this.client.Send(message);
+        }
+        public void conectar()
+        {            
+            this.client.Connect(this.uri);
+            this.onNotification("Connecting...");            
+
+            System.Threading.Thread beginReadThread = new System.Threading.Thread(new System.Threading.ThreadStart(beginRead));
+            beginReadThread.Start();
+                        
+        }
+
+        public void beginRead(){
+            String response = null;
+            while (true)
+            {
+                response = this.client.Receive<String>();
+                this.onNewMessage(response);            
+            }
+        }
+
+    }
+}
